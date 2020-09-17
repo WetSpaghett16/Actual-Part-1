2725c523b1a24e771b92aa321a0844f39bd69725
David Thomas <ThomDavi727@GoTVDSB.ca>
2020-09-17 12:53:27 -0400

    Add project files.

---
+++ Actual Part 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actual_Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DDDD   TTTTT");
+            Console.WriteLine("D    D   T");
+            Console.WriteLine("D    D   T");
+            Console.WriteLine("D    D   T");
+            Console.WriteLine("D    D   T");
+            Console.WriteLine("DDDD     T");
+        }
+    }
+}

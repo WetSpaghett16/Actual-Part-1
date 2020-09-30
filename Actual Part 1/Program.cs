7ead496bb6da2fd314ad725cd84cbeb9241452c9
David Thomas <ThomDavi727@GoTVDSB.ca>
2020-09-30 09:11:05 -0400

    added code

--- Actual Part 1/Program.cs
+++ Actual Part 1/Program.cs
@@ -5,17 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Actual_Part_1
+
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string name = "David";
+            string movie = "The Matrix";
+            string word = "THE";
+            string sentence = ($"My name is {name.ToLower()} and my favourite movie is {movie.ToUpper()}");
+
             Console.WriteLine("DDDD   TTTTT");
             Console.WriteLine("D    D   T");
             Console.WriteLine("D    D   T");
             Console.WriteLine("D    D   T");
             Console.WriteLine("D    D   T");
             Console.WriteLine("DDDD     T");
+
+            Console.WriteLine(sentence.Contains("THE"));
+
+            Console.WriteLine(sentence.Replace("a" , "@"));
+            Console.WriteLine(sentence.Replace("e", "3"));
+            Console.ReadLine();
         }
     }
 }
